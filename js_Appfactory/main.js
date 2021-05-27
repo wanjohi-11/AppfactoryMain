90d4608c6b618175e6d022203d0edb4c165acdf4
wanjohi-Maxwe11 <maxwell@gentium.tech>
2021-05-27 13:48:57 +0300

    App factory first week

---
+++ js_Appfactory/main.js
@@ -0,0 +1,20 @@
+// three ways of declaring a variable
+// way one -  using var can be manipulated to change
+var x = "Mercury";
+//way two -  using const, this means the var will not be manipulated
+const f = 40.5;
+//way three - using 
+let m = false;
+// alert(m);
+// document.writeln(x);
+
+//string methods
+// 1. CHARACTER AT --index 0, mainly assists at someone texting yes or no and
+// and use the first character of the answer
+document.writeln(x.charAt(0)+"<br>");
+//index of
+document.writeln(x.indexOf("r")+"<br>")
+//to lowercase
+document.writeln(x.toLowerCase(x)+"<br>")
+//to uppercase
+document.write(x.toUpperCase(x)+"<br>")
